c6200785b7f6248c99db6cd2058f19fde95da71c
Diana Fisher <diana.fisher@gmail.com>
2016-02-29 15:10:31 -0800

    Use memcache to send announcement when a conference is almost full.

--- Lesson_2/00_Conference_Central/models.py
+++ Lesson_2/00_Conference_Central/models.py
@@ -109,5 +109,10 @@ class BooleanMessage(messages.Message):
 
 class ConflictException(endpoints.ServiceException):
     """ConflictException -- exception mapped to HTTP 409 response"""
-    http_status = httplib.CONFLICT    
+    http_status = httplib.CONFLICT
+
+class StringMessage(messages.Message):
+    """StringMessage-- outbound (single) string message"""
+    data = messages.StringField(1, required=True)
+    
     
